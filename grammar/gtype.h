a67da5ceaca4c447f7fd597e626a46167970be2f
Max K <pyram3d@gmail.com>
2018-02-15 22:07:54 -0600

    Changed directory name. Added FLEX ref file.

---
+++ grammar/gtype.h
@@ -0,0 +1,90 @@
+#ifndef __GTYPE_HEADER
+#define __GTYPE_HEADER
+
+// Types
+#define PRIM_TYPE_STRING			10
+#define PRIM_TYPE_LIST				11
+#define PRIM_TYPE_SET				12
+#define PRIM_TYPE_I8				13
+#define PRIM_TYPE_I16				14
+#define PRIM_TYPE_I32				15
+#define PRIM_TYPE_I64				16
+#define PRIM_TYPE_UI8				17
+#define PRIM_TYPE_UI16				18
+#define PRIM_TYPE_UI32				19
+#define PRIM_TYPE_UI64				20
+#define PRIM_TYPE_F32				21
+#define PRIM_TYPE_F64				22
+#define PRIM_TYPE_BOOL				23
+#define PRIM_TYPE_VAR				24
+
+// Operators
+#define OPERATOR_ASSIGNMENT			30
+#define OPERATOR_DEEP_ASSIGNMENT	31
+#define OPERATOR_DEEP_COPY			32
+#define OPERATOR_SET_TO				33
+#define OPERATOR_SET_SKIP			34
+#define OPERATOR_BOOL_LESS			35
+#define OPERATOR_BOOL_LESS_OR_EQ	36
+#define OPERATOR_BOOL_GREAT			37
+#define OPERATOR_BOOL_GREAT_OR_EQ	38
+#define OPERATOR_BOOL_EQ			39
+#define OPERATOR_BOOL_NEQ			40
+#define OPERATOR_BOOL_NEG			41
+#define OPERATOR_LOGICAL_OR			42
+#define OPERATOR_LOGICAL_AND		43
+#define OPERATOR_INDEX				44
+#define OPERATOR_MULT				45
+#define OPERATOR_DIV				46
+#define OPERATOR_ADD				47
+#define OPERATOR_SUB				48
+#define OPERATOR_POW				49
+#define OPERATOR_LENGTH				50
+#define OPERATOR_MOD				51
+
+// Other Single Symbols
+#define SYM_DOUBLE_QUOTE			60
+#define SYM_SINGLE_QUOTE			61
+#define SYM_LIST_START				62
+#define SYM_LIST_END				63
+#define SYM_SET_START				64
+#define SYM_SET_END					65
+#define SYM_COMMA					66
+#define SYM_STR_CHAR				67
+#define SYM_PAR_OPEN				68
+#define SYM_PAR_CLOSE				69
+
+// Keywords
+#define KEY_FOR_LOOP				80
+#define KEY_IF						81
+//#define KEY_THEN					82
+#define KEY_ELSE					83
+#define KEY_RETURN					84
+#define KEY_BREAK					85
+#define KEY_WHILE					86
+
+// Others
+#define IDENTIFIER					100
+#define INT_NUMBER					101
+#define I64_NUMBER					102
+#define F32_NUMBER					103
+#define F64_NUMBER					104
+#define BOOL_TRUE					105
+#define BOOL_FALSE					106
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
